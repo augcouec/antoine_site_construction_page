c2e951d965b4aed650686f8585acddd69df5c765
augcouec <augcouec@gmail.com>
2022-03-23 18:31:46 +0100

    change title

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "antoine-page",
+    title: "Agence Bobard",
     htmlAttrs: {
       lang: "en",
     },
