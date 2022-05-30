ffbdaa837c19094b35f962bb9f10957c31d36888
augcouec <augcouec@gmail.com>
2022-05-30 12:13:52 +0200

    fixnode module

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,6 +34,7 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [],
+  devModules: ["@nuxtjs/eslint-module"],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
